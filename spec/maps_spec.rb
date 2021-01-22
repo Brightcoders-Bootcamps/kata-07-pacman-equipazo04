1171920157151da20b5781e5a460d3d2b1ad576c
OchoaHub <jochoa2@ucol.mx>
2021-01-22 00:02:38 -0600

    Add | Tests for the game

--- spec/maps_spec.rb
+++ spec/maps_spec.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require_relative '../app/maps'
 
 describe Maps do
-    maps = Maps.new(11,11)
+  #maps = Maps.new(11, 11)
 
-    it 'print the board' do
-        expect(maps.width).to be(11)
-        expect(maps.height).to be(11)
-    end
-
+  it 'print the board' do
+    expect(maps.width).to be(11)
+    expect(maps.height).to be(11)
+  end
 end
--- spec/pac_spec.rb
+++ spec/pac_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require_relative '../app/pac'
 
 describe Pac do
-    pacman = Pac.new(5,5)
+  #pacman = Pac.new(5, 5)
 
-    it 'default position' do
-        expect(pacman.posi_x).to be(5)
-        expect(pacman.posi_y).to be(5)
-    end
+  it 'default position' do
+    expect(pacman.posi_x).to be(5)
+    expect(pacman.posi_y).to be(5)
+  end
 end
